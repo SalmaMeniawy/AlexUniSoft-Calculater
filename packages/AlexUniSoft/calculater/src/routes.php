28db87fdd129c3cc19d32f61cc4485ca3e15043e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-09-03 18:25:07 +0200

    add routes of the controller

--- packages/AlexUniSoft/calculater/src/routes.php
+++ packages/AlexUniSoft/calculater/src/routes.php
@@ -2,3 +2,5 @@
 Route::get('test',function(){
     return "Hello from AlexUniSoft new package";
 });
+Route::get('add/{num1}/{num2}','AlexUniSoft\Calculater\CalculaterController@add');
+Route::get('sub/{num1}/{num2}','AlexUniSoft\Calculater\CalculaterController@subtract');
