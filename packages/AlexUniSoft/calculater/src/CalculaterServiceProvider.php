bcaec879a9fa4dc5d7eb269d3c31b5ef2eedfc51
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-09-03 18:39:25 +0200

    load Views from our package

--- packages/AlexUniSoft/calculater/src/CalculaterServiceProvider.php
+++ packages/AlexUniSoft/calculater/src/CalculaterServiceProvider.php
@@ -24,5 +24,6 @@ class CalculaterServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadViewsFrom(__DIR__.'/views','calculaterViews');
     }
 }
