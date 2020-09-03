21bba211722b90470689c564c1e9023dd63b9e07
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-09-03 18:39:03 +0200

    change add method to get add view

--- packages/AlexUniSoft/calculater/src/CalculaterController.php
+++ packages/AlexUniSoft/calculater/src/CalculaterController.php
@@ -7,7 +7,9 @@ use App\Http\Controllers\Controller;
 class CalculaterController extends Controller
 {
     public function add($num1 , $num2){
-        return $num1 + $num2;
+
+        $result =  $num1 + $num2;
+        return view('calculaterViews::add')->with('result',$result);
     }
     public function  subtract($num1 , $num2){
         return $num1 - $num2;
