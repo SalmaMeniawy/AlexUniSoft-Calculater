0ba5cbde5ffb7db6c1aed76fa0c983d45d11e264
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-09-03 18:06:14 +0200

    create CalculaterServiceProvider for our package

---
+++ packages/AlexUniSoft/calculater/src/CalculaterServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AlexUniSoft\Calculater;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculaterServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
